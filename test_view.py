1ff0bd797b198ab3edf34d038b1ac18531e968e2
Joseph Jennings <jojennin@mines.edu>
2013-09-22 09:47:11 -0500

    Test Script

---
+++ test_view.py
@@ -0,0 +1,17 @@
+#! /Users/joe/Library/Enthought/Canopy_64bit/User/bin/python
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from Lasview import plotlas
+from Las_view_all import plotAllLas
+
+#plotlas("/Users/joe/Desktop/LAS_Viewer/DataSets/Well Log/CD Files/LAS_log_files/Deeper_LAS_files/49025106780000_282182.LAS", "GRD")
+
+plotAllLas("/Users/joe/Desktop/LAS_Viewer/DataSets/Well Log/CD Files/LAS_log_files/Deeper_LAS_files/49025106780000_282182.LAS", "GRD")
+
+
+
+
+
